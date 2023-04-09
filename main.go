0cd715b96133b31ca809017e2f2e75095f4af66c
atsu12 <70347176+atsu12@users.noreply.github.com>
2023-04-09 20:38:28 +0000

    Update main.go

--- main.go
+++ main.go
@@ -1,2 +1,18 @@
 I am glad to be here today
 today is Easter Sunday
+
+The father of two British-Israeli sisters killed in a shooting in the occupied West Bank embraced their 
+bodies while mourners sang songs of grief at their funeral on Sunday.
+
+Maia and Rina Dee, 20 and 15, were killed on Friday when suspected Palestinian gunmen opened fire on them 
+in their car in the Jordan Valley.
+
+Their mother, Leah, is in a critical condition following surgery.
+
+The attack came amid soaring Israeli-Palestinian tensions and violence.
+
+The low rhythmic songs swelled and swayed with the crowd, who were packed beneath the white rafters in 
+the prayer hall at a cemetery in the settlement of Kfar Etzion.
+
+Many at the funeral were teenagers - some from the school Rina went to. At the front, by a low podium, 
+the family gathered, talking together and holding each other for long moments in silence.
